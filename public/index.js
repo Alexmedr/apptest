8a0c0856b20059d4d63dfbc5bc8b68ec26916c16
Alejandro <alejandromedrano90@gmail.com>
2022-02-04 16:15:20 -0600

    App-web-tetst version 1.2.0

---
+++ public/index.js
@@ -0,0 +1,63 @@
+var students = [
+    {
+        lname: "Medrano",
+        name: "Alejandro",
+        age: 23,
+        birthday: {
+            year: 1998,
+            month: "August",
+            day: 23
+        },
+        career: "IDYGS"
+    },
+    {
+        lname: "Hernandez",
+        name: "Emir",
+        age: 22,
+        birthday: {
+            year: 2000,
+            month: "November",
+            day: 17
+        },
+        career: "IDYGS"
+    },
+    {
+        lname: "Resendiz",
+        name: "Homero",
+        age: 22,
+        birthday: {
+            year: 1999,
+            month: "September",
+            day: 11
+        },
+        career: "IDYGS"
+    },
+    {
+        lname: "Villicaña",
+        name: "Adolfo",
+        age: 26,
+        birthday: {
+            year: 1995,
+            month: "October",
+            day: 11,
+        },
+        Career: "IDYGS"
+    }
+]
+
+var table = document.getElementsByTagName("table")[0];
+for (var i = 0; i < students.length; i++ ) {
+    var templ = document.getElementById("tbodytempl").content.cloneNode(true);
+    var tdnumber = templ.querySelector("span[name='tbodynumber']");
+    var tdname = templ.querySelector("span[name='tbodyname']");
+    var tdage = templ.querySelector("span[name='tbodyage']");
+    var tdcareer = templ.querySelector("span[name='tbodycareer']");
+
+    
+    tdnumber.innerText = i + 1; 
+    tdname.innerText = students[i].lname + " " + students[i].name;
+    tdage.innerText = students[i].age;
+    tdcareer.innerText = students[i].career;
+
+    table.append(templ);
+}
